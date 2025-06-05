8521d07705a1e5ba076884098eda2ef7eafe5b64
MilaneUpdate <202502984383@alunos.estacio.br>
2025-06-05 13:49:16 +0000

    int main(){     int idade = 30;     int quantidade = 1;     float altura = 1.59;     double peso = 54;     char letra = 'M';     char nome[20] = "Milane";
        pintf("A idade do %s é: d%\n", nome, idade);
        printf("A altura é %e\n", altura);
        printf("A opção é :%c\n", opção);

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(){
+    int idade = 30;
+    int quantidade = 1;
+    float altura = 1.59;
+    double peso = 54;
+    char letra = 'M';
+    char nome[20] = "Milane";
+
+    pintf("A idade do %s é: d%\n", nome, idade);
+    printf("A altura é %e\n", altura);
+    printf("A opção é :%c\n", opção);
+
+
+
+
+
+}
